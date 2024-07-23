54e76883fbb6f103afc0a711d836a41c7be25a1a
Yousef Elsayed <yousef4511@outlook.com>
2024-07-23 11:37:44 +0300

    Style personal info section

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import PersonalInfoData from "../types/PersonalInfoData";
 import PersonalInfoPreview from "./PersonalInfoPreview";
-import PersonalInfoForm from "./PersonalInfoForm";
 
 function PersonalInfo() {
   // TODO: Load from Local Storage in future;
   const [data, setData] = useState<PersonalInfoData>({});
 
-  if (data) {
-    return <PersonalInfoPreview info={data} />;
-  }
-
-  return <PersonalInfoForm />;
+  return (
+    <div className="bg-white p-4 rounded-3xl shadow-md">
+      <PersonalInfoPreview info={data} />
+    </div>
+  );
 }
 
 export default PersonalInfo;
